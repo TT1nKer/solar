4df7762354b20184388b5bba9462ccdfeb6dc1bb
TT1nker <hostsjimi@gmail.com>
2026-07-30 12:12:36 +0800

    test: define Solar external consumer contract

---
+++ include/solar/version.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string_view>
+
+namespace solar {
+
+inline constexpr std::string_view version{"0.2.0-alpha.1"};
+inline constexpr std::string_view physics_contract{"relativity-v3-phase2"};
+
+} // namespace solar
